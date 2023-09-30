ae3c9daf63dc413ca26182c56dfa12bab7b1960c
Monica Jerobon <monijerobon@gmail.com>
2023-09-30 06:37:36 +0300

    Nested while loop

---
+++ while.py
@@ -0,0 +1,18 @@
+# list = []
+# i = 1
+# while i in range(1,100):
+#     if i % 3 == 1 or i % 5 == 1:
+#         print(i)
+#         i += 1
+#         list.append(i)
+num = 1
+
+while num <= 100:
+    if num % 3 != 0 and num % 5 != 0:
+        print(num)
+    num += 1
+
+        
+        
+    
+    
